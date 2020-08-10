96dded35aaa8827641df540689674cf7f1173bb4
dxxglas <mcavalcante.douglas@gmail.com>
2020-08-10 17:36:16 -0300

    f-form: configuração de conexão com o firebase

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from './../comments.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  commentName: string;
+  commentMessage: string;
+
+  constructor(
+    private commentsService: CommentsService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  AddComment() {
+    console.log('here')
+    let record = {};
+    record['Name'] = this.commentName;
+    record['Message'] = this.commentMessage;
+    this.commentsService.create_newComment(record).then(resp => {
+      this.commentName="";
+      this.commentMessage = "";
+      console.log(resp);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
 }
