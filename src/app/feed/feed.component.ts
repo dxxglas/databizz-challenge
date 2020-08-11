8325f3c617b1b27b9fef849033b34cd084b17ef7
dxxglas <mcavalcante.douglas@gmail.com>
2020-08-11 10:34:12 -0300

    f-feed: adicionando estrutura

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from './../comments.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  comments: any;
+
+  constructor(
+    private commentsService: CommentsService
+  ) { }
 
   ngOnInit(): void {
+    this.commentsService.read_Comments().subscribe(data => {
+      this.comments = data.map(e => {
+      return{
+        id: e.payload.doc.id,
+        name: e.payload.doc.data()['Name'],
+        message: e.payload.doc.data()['Message'],
+      };
+      })
+      console.log(this.comments);
+      });
   }
 
 }
